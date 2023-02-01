6e4aaa0c2db5d7060964eb6e2a40492f0fb7583f
PELE <PELE@ZEA3916>
2023-02-01 12:30:33 +0100

    Add project files.

---
+++ PizzaLib/model/Pizza.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaLib.model
+{
+    public class Pizza
+    {
+        private int _id;
+        private string _name;
+        private double _pris;
+
+        public int Id { get => _id; 
+            set { 
+                // betingelse
+                if( !(value >= 0) )
+                {
+                    throw new ArgumentException("Id skal være positiv");
+                }
+
+                _id = value;
+            
+            }
+        }
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                // betingelse
+                if (!(value is not null))
+                {
+                    throw new ArgumentNullException("Der skal være et navn");
+                }
+                if (!(value.Length >= 5))
+                {
+                    throw new ArgumentException("Navn skal være mindst 5 tegn");
+                }
+
+                _name = value;
+            }
+        }
+        public double Pris
+        {
+            get => _pris;
+            set {
+                // betingelse
+                if (!(40<=value && value<=120))
+                {
+                    throw new ArgumentException("Pris skal være mellem 40 og 120");
+                }
+
+                _pris = value; }
+        }
+
+        public Pizza(int id, string name, double pris) { 
+            _id = id;
+            _name = name;
+            _pris = pris;
+        
+        }
+
+        public Pizza() : this(0, "dummy",100)
+        { }
+
+        public override string ToString()
+        {
+            return "kommer senere";
+        }
+
+    }
+}
---
+++ PrøvPizza/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PizzaLib.model;
+
+Console.WriteLine("Hello, World!");
+
+
+Pizza p = new Pizza(2, "vesuio", 55);
+
+Console.WriteLine(p);
+
+
+try
+{
+    //p.Id = -1;
+    p.Pris = 39;
+}
+catch(ArgumentException ae)
+{
+    Console.WriteLine(ae.Message);
+}
