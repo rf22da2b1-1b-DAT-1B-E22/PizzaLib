05bededc755b85a55157d899c213573d15b44338
Peter Levinsky <PELE@zealand.dk>
2023-02-17 13:05:47 +0100

    Med pizza og enum

---
+++ PizzaLib/model/PizzaEnum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaLib.model
+{
+    public enum PizzaType { Almindelig, DeepPan, Lukket }
+
+    public  class PizzaEnum:Pizza
+    {
+        public PizzaType Slags { get; set; }
+
+        public PizzaEnum(int id, string name, double pris, PizzaType slags):base(id,name,pris)
+        {
+            Slags = slags;
+        }
+        public PizzaEnum() : base()
+        {
+            Slags = PizzaType.Almindelig;
+        }
+
+    }
+}
