113f6dfa48b1f834c426fc0107f38ce808a3c883
Peter Levinsky <PELE@zealand.dk>
2023-02-17 11:24:51 +0100

    Ok nu med dicionary

--- PizzaLib/model/PizzaRepositoryDictionary.cs
+++ PizzaLib/model/PizzaRepositoryDictionary.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace PizzaLib.model
 {
-    internal class PizzaRepositoryDictionary
+    public class PizzaRepositoryDictionary : IPizzaRepository
     {
+        private readonly Dictionary<int, Pizza> _repository = new Dictionary<int, Pizza>();
+
+        public Pizza Create(Pizza pizza)
+        {
+            _repository.Add(pizza.Id, pizza);
+            return pizza;
+        }
+
+        public List<Pizza> GetAll()
+        {
+            return new List<Pizza>(_repository.Values);
+        }
+
+        public Pizza Get(int id)
+        {
+            if (_repository.ContainsKey(id))
+            {
+                return _repository[id];
+            }
+            
+            throw new KeyNotFoundException();
+        }
     }
 }
